cc3e0b2237117ad5ed3b80946d4842bb5f96306e
Ronnie Baez <ronnie.baez@gmail.com>
2018-10-08 18:09:31 -0400

    adds retry prompt to end screen with timer

--- pong.py
+++ pong.py
@@ -17,12 +17,15 @@ BOOL = {True: 1, False: 0}
 SPEED = 3
 GAME_LIMIT = 5
 PLAYER_WON = None
+REMATCH_TIMER = 10
+RUNNING = True
 
 # Screen
 pygame.display.set_caption("Pong")
 screen = pygame.display.set_mode((800, 480), 0, 32)
 width, height = pygame.display.get_surface().get_size()
-screen.fill((0,0,0))
+screen.fill((0, 0, 0))
+
 
 # Objects
 class Ball:
@@ -31,6 +34,7 @@ class Ball:
         self.y = y
         self.vx = vx
         self.vy = vy
+
 
 class Player:
     def __init__(self, x, y, score):
@@ -43,8 +47,39 @@ ball = Ball(width/2, height/2, -SPEED, SPEED)
 player1 = Player(10, height/2, 0)
 player2 = Player(width - 30, height/2, 0)
 
+
+def initial_game_state(
+        white=(255, 255, 255),
+        is_down=pygame.key.get_pressed(),
+        bool_value={True: 1, False: 0}, speed=3,
+        game_limit=5, player_won=None, rematch_timer=10, running=True):
+    global WHITE
+    global ISDOWN
+    global BOOL
+    global SPEED
+    global GAME_LIMIT
+    global PLAYER_WON
+    global REMATCH_TIMER
+    global RUNNING
+    global ball
+    global player1
+    global player2
+
+    WHITE = white
+    ISDOWN = is_down
+    BOOL = bool_value
+    SPEED = speed
+    GAME_LIMIT = game_limit
+    PLAYER_WON = player_won
+    REMATCH_TIMER = rematch_timer
+    RUNNING = running
+    ball = Ball(width/2, height/2, -SPEED, SPEED)
+    player1 = Player(10, height/2, 0)
+    player2 = Player(width - 30, height/2, 0)
+
+
 def update():
-    global running
+    global RUNNING
     global SPEED
     global PLAYER_WON
 
@@ -52,27 +87,35 @@ def update():
     ISDOWN = pygame.key.get_pressed()
 
     if player1.score == GAME_LIMIT:
-        running = False
+        RUNNING = False
         PLAYER_WON = 1
         return
     elif player2.score == GAME_LIMIT:
-        running = False
+        RUNNING = False
         PLAYER_WON = 2
         return
 
-    player1.y = player1.y + (BOOL[ISDOWN[pygame.K_a]] - BOOL[ISDOWN[pygame.K_q]]) * 7
-    player2.y = player2.y + (BOOL[ISDOWN[pygame.K_DOWN]] - BOOL[ISDOWN[pygame.K_UP]]) * 7
+    player1.y = player1.y + \
+        (BOOL[ISDOWN[pygame.K_a]] - BOOL[ISDOWN[pygame.K_q]]) * 7
+    player2.y = player2.y + \
+        (BOOL[ISDOWN[pygame.K_DOWN]] - BOOL[ISDOWN[pygame.K_UP]]) * 7
 
-    if player1.y < 100: player1.y = 100
-    if player1.y > (height - 60): player1.y = height - 60
-    if player2.y < 100: player2.y = 100
-    if player2.y >  (height - 60): player2.y = height - 60
+    if player1.y < 100:
+        player1.y = 100
+    if player1.y > (height - 60):
+        player1.y = height - 60
+    if player2.y < 100:
+        player2.y = 100
+    if player2.y > (height - 60):
+        player2.y = height - 60
 
     ball.x = ball.x + ball.vx
     ball.y = ball.y + ball.vy
 
-    if abs(ball.y) >= (height - 20): ball.vy = -ball.vy
-    if abs(ball.y) <= 60: ball.vy = -ball.vy
+    if abs(ball.y) >= (height - 20):
+        ball.vy = -ball.vy
+    if abs(ball.y) <= 60:
+        ball.vy = -ball.vy
 
     if ball.x < 40 and ball.x > 20 and abs(player1.y - ball.y) < 60:
         SPEED = SPEED + 0.2
@@ -87,7 +130,7 @@ def update():
         ball.vx = -SPEED
         ball.vy = -SPEED
         player2.score = player2.score + 1
-	
+
     if ball.x > (width - 40) and ball.x < (width - 20) and abs(player2.y - ball.y) < 60:
         SPEED = SPEED + 0.5
         ball.x = (width - 40)
@@ -104,7 +147,8 @@ def update():
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            running = False
+            RUNNING = False
+
 
 def draw():
     title = fontLG.render("PONG", False, WHITE)
@@ -123,22 +167,71 @@ def draw():
     score2 = fontLG.render(str(player2.score), False, WHITE)
     screen.blit(score2, (width - score2.get_size()[0] - 10, 10))
     instructions2 = fontSM.render("P2 Keys: UP and DOWN", False, WHITE)
-    screen.blit(instructions2, (width - score2.get_size()[0] - 20 - instructions2.get_size()[0], 18))
+    screen.blit(instructions2, (width - score2.get_size()
+                                [0] - 20 - instructions2.get_size()[0], 18))
+
 
 def drawGameEnd():
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
     title = fontLG.render("Player {0} won!".format(PLAYER_WON), False, WHITE)
-    screen.blit(title, (width/2 - title.get_size()[0]/2, 10))
+    screen.blit(title, (width/2 - title.get_size()[0]/2, height/4))
+    message = fontSM.render("Up for a rematch? (y / n)", False, WHITE)
+    screen.blit(message, (width/2 - message.get_size()[0]/2, height/2))
+    message = fontSM.render("({0})".format(REMATCH_TIMER), False, WHITE)
+    screen.blit(message, (width/2 - message.get_size()[0]/2, 3*height/4))
     pygame.display.flip()
 
-running = True
-while running:
-    screen.fill((0,0,0))
-    update()
-    draw()
-    clock.tick(60)
-    pygame.display.flip()
 
-drawGameEnd()
+def main():
+    global RUNNING
+    initial_game_state()
+    while RUNNING:
+        print("Game running")
+        screen.fill((0, 0, 0))
+        update()
+        draw()
+        clock.tick(60)
+        pygame.display.flip()
+    if not game_end():
+        print("Game restarted")
+        main()
+
+
+def game_end():
+    global REMATCH_TIMER
+    result = True
+    while True:
+        # Retry or not
+        events = pygame.event.get()
+        selected = None
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_y:
+                    print("Yes pressed!")
+                    selected = "y"
+                    break
+                elif event.key == pygame.K_n:
+                    selected = "n"
+                    break
+        # Has the timer ended?
+        if REMATCH_TIMER <= 0:
+            result = True
+            break
+        # Check selected
+        if selected == "y":
+            result = False
+            break
+        elif selected == "n":
+            result = True
+            break
+        # Keeps counting down
+        REMATCH_TIMER -= 1
+        drawGameEnd()
+        pygame.time.wait(1000)
+    return result
+
+
+if RUNNING:
+    main()
+
 pygame.quit()
-pygame.time.wait(2500)
